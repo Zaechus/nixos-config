cf8bd6d5c025901cf9a1f6cc4238cee9bf8c5f14
Maxwell Anderson <maxwell.anderson.public@protonmail.com>
2022-03-24 01:14:32 -0600

    remove status.sh

--- modules/sway/status.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#$(awk '/^Mem/ {print $3}' <(free -h))
-echo "$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)  $(cat /sys/class/power_supply/BAT0/capacity)%  $(date +'%Y-%m-%d %H:%M:%S')"
