6777454035e241356128d9b577cd6243aff9a6e9
Zaechus <maxwell.anderson.public@protonmail.com>
2022-03-28 10:38:02 -0600

    backup world file

--- hosts/gentoopad/postinst.sh
+++ hosts/gentoopad/postinst.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -e
 
-# Custom make.conf
-cp configs/make.conf /etc/portage/make.conf
-emerge -avuDN @world
-
 # Cron
 emerge -qn sys-process/cronie
 systemctl enable cronie
@@ -13,9 +9,3 @@ emerge -qn app-admin/sudo app-shells/zsh app-shells/zsh-completions app-shells/g
 sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
 useradd -m -G users,wheel,audio,video,portage,systemd-journal -s /bin/zsh zaechus
 passwd zaechus
-
-# Packages
-mkdir -p /etc/portage/package.accept_keywords/
-mkdir -p /etc/portage/package.use/
-echo dev-libs/bemenu > /etc/portage/package.accept_keywords/bemenu
-emerge -qn gui-wm/sway app-misc/neofetch x11-terms/alacritty dev-libs/bemenu app-editors/neovim
