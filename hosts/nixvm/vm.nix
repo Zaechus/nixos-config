1a9e0a19498373a9947ff40c96807a9ab425c921
Maxwell Anderson <maxwell.anderson.public@protonmail.com>
2022-03-28 11:07:03 -0600

    wlr renderer env var for vm

--- hosts/nixvm/vm.nix
+++ hosts/nixvm/vm.nix
@@ -2,4 +2,8 @@
   home-manager.users.zaechus.wayland.windowManager.sway.config.output = {
     Virtual-1 = { bg = "${../../modules/sway/background.jpg} fill"; };
   };
+
+  environment.sessionVariables = rec {
+    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+  };
 }
