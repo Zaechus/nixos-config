5ad3ff6923a83845e8cfa699b03bbb17da49c980
Zaechus <maxwell.anderson.public@protonmail.com>
2022-03-27 21:29:12 -0600

    fix status.sh for non vm

--- modules/sway/status.sh
+++ modules/sway/status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)  $(cat /sys/class/power_supply/BAT0/capacity && echo %)  $(date +'%Y-%m-%d %H:%M:%S')"
+echo "$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)  $(cat /sys/class/power_supply/BAT0/capacity | tr -d \\n && echo %)  $(date +'%Y-%m-%d %H:%M:%S')"
