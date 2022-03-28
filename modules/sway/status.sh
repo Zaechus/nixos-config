cbf1f3f6dd0c288060594adc2f19de6e2a681e93
Maxwell Anderson <maxwell.anderson.public@protonmail.com>
2022-03-28 11:13:12 -0600

    improve status.sh

--- modules/sway/status.sh
+++ modules/sway/status.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo "$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)  $(cat /sys/class/power_supply/BAT0/capacity | tr -d \\n && echo %)  $(date +'%Y-%m-%d %H:%M:%S')"
+[[ -f /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor ]] && \
+    cpugov="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)  "
+
+[[ -f /sys/class/power_supply/BAT0/capacity ]] && \
+    battery="$(cat /sys/class/power_supply/BAT0/capacity | tr -d \\n)%  "
+
+echo "$cpugov$battery$(date +'%Y-%m-%d %H:%M:%S')"
