d8c71635300c9849961fac3ef50194124f709251
Zaechus <maxwell.anderson.public@protonmail.com>
2022-04-01 00:03:57 -0600

    use tokyonight on gentoopad

--- hosts/gentoopad/home.nix
+++ hosts/gentoopad/home.nix
@@ -4,6 +4,7 @@
   imports =
     [
       ../../modules/home.nix
+      ../../themes/tokyonight.nix
       ../../modules/graphical-home.nix
       ../../modules/git
       ../../modules/thinkpad-home.nix
