21d6376c98a075debfac1a943da01b65b7cd9d14
Zaechus <maxwell.anderson.public@protonmail.com>
2022-04-10 15:30:30 -0600

    finally, default sort buffers by tabs

--- modules/neovim/setup.lua
+++ modules/neovim/setup.lua
@@ -1,12 +1,13 @@
-require('lualine').setup()
+require'lualine'.setup()
 require'nvim-tree'.setup {
   filters = { dotfiles = true, },
   hijack_unnamed_buffer_when_opening = false,
 }
-require("bufferline").setup {
+require'bufferline'.setup {
   options = {
-    --mode = "tabs",
-    numbers = "buffer_id",
+    --mode = 'tabs',
+    numbers = 'buffer_id',
     always_show_bufferline = false,
+    sort_by = 'tabs';
   }
 }
