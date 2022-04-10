99c059e16211222f4fd87c17220cbb5f6b60238b
Zaechus <maxwell.anderson.public@protonmail.com>
2022-04-10 03:17:19 -0600

    nvim tree hijack unnamed buffer

--- modules/neovim/setup.lua
+++ modules/neovim/setup.lua
@@ -1,6 +1,7 @@
 require('lualine').setup()
 require'nvim-tree'.setup {
   filters = { dotfiles = true, },
+  hijack_unnamed_buffer_when_opening = false,
 }
 require("bufferline").setup {
   options = {
