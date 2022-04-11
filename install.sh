bfe472faaa0dd0fe1b9c88242706dd7a01b08cf4
Zaechus <maxwell.anderson.public@protonmail.com>
2022-04-11 16:33:47 -0600

    try nixos install with swapfile

--- install.sh
+++ install.sh
@@ -21,26 +21,20 @@ cryptpass=$(confirm_password "encryption password")
 if [[ $my_disk == *"nvme"* ]]; then
     part1="$my_disk"p1
     part2="$my_disk"p2
-    part3="$my_disk"p3
 else
     part1="$my_disk"1
     part2="$my_disk"2
-    part3="$my_disk"3
 fi
 
 # Partition
-printf "label: gpt\n,550M,U\n,4G,S\n,,L\n" | sfdisk $my_disk
+printf "label: gpt\n,550M,U\n,,L\n" | sfdisk $my_disk
 
 # Encrypt
-yes $cryptpass | cryptsetup -q luksFormat $part3
 yes $cryptpass | cryptsetup -q luksFormat $part2
-yes $cryptpass | cryptsetup open $part3 root
-yes $cryptpass | cryptsetup open $part2 swap
+yes $cryptpass | cryptsetup open $part2 root
 
 # Format
 mkfs.fat -F 32 $part1
-
-mkswap /dev/mapper/swap
 
 mkfs.btrfs /dev/mapper/root
 mkdir -p /mnt
@@ -57,12 +51,16 @@ mount -o compress=zstd,subvol=home /dev/mapper/root /mnt/home
 mkdir /mnt/boot
 mount $part1 /mnt/boot
 
-swapon /dev/mapper/swap
+# Swapfile
+mkdir /mnt/var
+truncate -s 0 /mnt/var/swapfile
+chattr +C /mnt/var/swapfile
+btrfs property set /mnt/var/swapfile compression none
+fallocate -l 4G /mnt/var/swapfile
+chmod 0600 /mnt/var/swapfile
+mkswap /mnt/var/swapfile
 
 # NixOS
 nixos-generate-config --root /mnt
-printf "{ boot.initrd.luks.devices.\"swap\".device = \"/dev/disk/by-uuid/$(blkid $part2 -o value -s UUID)\"; }\n" > /mnt/etc/nixos/swap-configuration.nix
-sed -i 's/.*hardware-configuration.nix.*/    \/etc\/nixos\/hardware-configuration.nix/' /mnt/etc/nixos/configuration.nix
-sed -i '/\/etc\/nixos\/hardware-configuration.nix/ s/$/ \/etc\/nixos\/swap-configuration.nix/' /mnt/etc/nixos/configuration.nix
 
 nixos-install
