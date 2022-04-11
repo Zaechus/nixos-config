2b48d5a477b6390d0855964388fec0e31b5b3046
Zaechus <maxwell.anderson.public@protonmail.com>
2022-04-11 16:14:09 -0600

    add starship to nushell

--- modules/nushell/default.nix
+++ modules/nushell/default.nix
@@ -1,8 +1,12 @@
 {
+  programs.starship.enable = true;
+
   programs.nushell = {
     enable = true;
+
     settings = {
       edit_mode = "vi";
+      prompt = ''STARSHIP_SHELL="nu" starship prompt'';
     };
   };
 }
