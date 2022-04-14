a1c14d3dbf7dcc8ad3b57d7d4f34974f02354a58
Zaechus <maxwell.anderson.public@protonmail.com>
2022-04-13 21:34:05 -0600

    theme tab bar qutebrowser

--- modules/qutebrowser/default.nix
+++ modules/qutebrowser/default.nix
@@ -6,8 +6,17 @@
 
     extraConfig = builtins.readFile ./config.py +
       ''
-      c.colors.tabs.selected.odd.bg = '${config.theme.bg}'
-      c.colors.tabs.selected.even.bg = '${config.theme.bg}'
+      c.colors.tabs.selected.odd.fg = '#ffffff'
+      c.colors.tabs.selected.even.fg = '#ffffff'
+      c.colors.tabs.odd.fg = '${config.theme.fg}'
+      c.colors.tabs.even.fg = '${config.theme.fg}'
+
+      c.colors.tabs.selected.odd.bg = '${config.theme.color}'
+      c.colors.tabs.selected.even.bg = '${config.theme.color}'
+      c.colors.tabs.odd.bg = '${config.theme.bg}'
+      c.colors.tabs.even.bg = '${config.theme.bg}'
+
+      c.colors.webpage.bg = '${config.theme.bg}'
     '';
   };
 }
