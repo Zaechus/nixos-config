6c8e8b8e5d0531d326ad0ebdd34936da7bf90e41
Zaechus <maxwell.anderson.public@protonmail.com>
2022-05-06 10:22:01 -0600

    fix ovmf paths in qemu-boot alias; use variable smp and m in base qemu alias

--- modules/virt-manager/default.nix
+++ modules/virt-manager/default.nix
@@ -6,7 +6,7 @@
   environment.systemPackages = with pkgs; [ virt-manager ];
 
   environment.shellAliases = {
-    qemu-boot-bios = "qemu-system-x86_64 -enable-kvm -cpu host -smp 12 -m 8G -vga virtio --display gtk,full-screen=on,show-cursor=on";
-    qemu-boot = "qemu-boot-bios -drive if=pflash,format=raw,unit=0,readonly=on,file=/usr/share/edk2-ovmf/OVMF_CODE.fd -drive if=pflash,format=raw,unit=1,file=OVMF_VARS.fd";
+    qemu-boot-bios = "qemu-system-x86_64 -enable-kvm -cpu host -smp $(($(nproc)-4)) -m $(($(free -m | awk 'NR==2 { print $2 }')/2))M -vga virtio --display gtk,full-screen=on,show-cursor=on";
+    qemu-boot = "qemu-boot-bios -drive if=pflash,format=raw,unit=0,readonly=on,file=/run/libvirt/nix-ovmf/OVMF_CODE.fd -drive if=pflash,format=raw,unit=1,file=OVMF_VARS.fd";
   };
 }
