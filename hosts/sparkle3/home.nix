08f86f28d48b8b2692c70d23b2fd59478eb1a003
Zaechus <maxwell.anderson.public@protonmail.com>
2022-06-01 23:23:56 -0600

    remove polymc from sparkles

--- hosts/sparkle3/home.nix
+++ hosts/sparkle3/home.nix
@@ -13,8 +13,6 @@
   home.username = "lizzie";
   home.homeDirectory = "/home/lizzie";
 
-  home.packages = with pkgs; [ polymc ];
-
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
