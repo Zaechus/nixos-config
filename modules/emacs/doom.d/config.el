fdee589dff0f78dfe805d390b74328410be0c33e
Zaechus <maxwell.anderson.public@protonmail.com>
2022-06-10 17:03:14 -0600

    remove more doom comments

--- modules/emacs/doom.d/config.el
+++ modules/emacs/doom.d/config.el
@@ -7,23 +7,9 @@
 (setq user-full-name "Maxwell Anderson"
       user-mail-address "maxwell.anderson.public@protonmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 (setq doom-font (font-spec :family "Fira Code Nerd Font" :size 12)
       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 
 (setq doom-theme 'doom-gruvbox)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
