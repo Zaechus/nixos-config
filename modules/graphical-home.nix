aaf04df9962a3e75e8a23badb618bee6b4a35abe
Zaechus <maxwell.anderson.public@protonmail.com>
2022-06-16 01:56:59 -0600

    use gammastep service for blue light filter on wayland

--- modules/graphical-home.nix
+++ modules/graphical-home.nix
@@ -13,4 +13,11 @@
     mime.enable = true;
     mimeApps.enable = true;
   };
+
+  # blue light filter
+  services.gammastep = {
+    enable = true;
+    dawnTime = "05:30-06:30";
+    duskTime = "08:30-09:30";
+  };
 }
