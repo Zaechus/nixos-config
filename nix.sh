7b82353c28a43330654c3daed19d1b5da492115c
Zaechus <maxwell.anderson.public@protonmail.com>
2022-07-14 22:33:57 -0600

    oops dont pipe sed

--- nix.sh
+++ nix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-sed -i "/^experimental-features.*/d" | /etc/nix/nix.conf
+sed -i "/^experimental-features.*/d" /etc/nix/nix.conf
 printf "experimental-features = nix-command flakes\n" >> /etc/nix/nix.conf
 
 nix build --no-link ".#homeConfigurations.$1.activationPackage"
