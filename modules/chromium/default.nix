5d2909fbd11630e66bc8a50f8c2244352b6905e9
Zaechus <maxwell.anderson.public@protonmail.com>
2022-08-04 22:13:54 -0600

    add svg default mime to brave

--- modules/chromium/default.nix
+++ modules/chromium/default.nix
@@ -15,5 +15,6 @@
   xdg.mimeApps.defaultApplications = {
     "text/html" = "brave-browser.desktop"; # for some reason this isn't a default
     "application/pdf" = "brave-browser.desktop";
+    "image/svg+xml" = "brave-browser.desktop";
   };
 }
