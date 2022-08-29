ea5bdfdb0dca2693ae97391450f7ba7f64673028
Zaechus <maxwell.anderson.public@protonmail.com>
2022-08-29 14:43:03 -0600

    use lld linker for rust by default

--- modules/dev/rust/home.nix
+++ modules/dev/rust/home.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 {
+  home.file.".cargo/config.toml".text = ''
+    [target.x86_64-unknown-linux-gnu]
+    rustflags = [ "-C", "link-arg=-fuse-ld=lld" ]
+  '';
+
   programs.neovim.plugins = with pkgs.vimPlugins; [
     rust-tools-nvim
     coc-rust-analyzer
