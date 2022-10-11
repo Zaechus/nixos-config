f0c310ce53e1e0ee7d3b998e356c54d90f6ea1e8
Zaechus <maxwell.anderson.public@protonmail.com>
2022-10-11 12:14:32 -0600

    add dosbox config with max cycles, add wc1 conf

--- modules/games/dosbox/default.nix
+++ modules/games/dosbox/default.nix
@@ -4,5 +4,7 @@
   ];
 
   home.file.".config/dosbox/dosbox-staging.conf".source = ./dosbox-staging.conf;
+  home.file.".config/dosbox/max.conf".source = ./max.conf;
   home.file.".config/dosbox/gauntlet.conf".source = ./gauntlet.conf;
+  home.file.".config/dosbox/warcraft.conf".source = ./warcraft.conf;
 }
