0a171c02b3175c8734a6c180eb1d4d87c602fbf0
Zaechus <zaechus@proton.me>
2022-11-09 23:18:49 -0700

    use choice menu for dosbox

--- modules/games/dosbox/default.nix
+++ modules/games/dosbox/default.nix
@@ -4,8 +4,4 @@
   ];
 
   home.file.".config/dosbox/dosbox-staging.conf".source = ./dosbox-staging.conf;
-  home.file.".config/dosbox/max.conf".source = ./max.conf;
-  home.file.".config/dosbox/arena.conf".source = ./arena.conf;
-  home.file.".config/dosbox/gauntlet.conf".source = ./gauntlet.conf;
-  home.file.".config/dosbox/warcraft.conf".source = ./warcraft.conf;
 }
