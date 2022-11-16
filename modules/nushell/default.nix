991d86e87079514a08c6d78a8fa63226736b6dd2
Zaechus <zaechus@proton.me>
2022-11-16 16:50:24 -0700

    conditional custom prompt

--- modules/nushell/default.nix
+++ modules/nushell/default.nix
@@ -1,6 +1,20 @@
 { config, lib, pkgs, ... }:
 
 let
+  PROMPT =
+    if config.programs.starship.enable == false then ''
+      def create_left_prompt [] {
+        $"\n(ansi blue_bold)($env.PWD | str replace /home/zaechus ~)(ansi reset)(ansi green_bold)\n〉"
+      }
+
+      let-env PROMPT_COMMAND = { create_left_prompt }
+      let-env PROMPT_COMMAND_RIGHT = {""}
+
+      let-env PROMPT_INDICATOR = { "" }
+      let-env PROMPT_INDICATOR_VI_INSERT = { ": " }
+      let-env PROMPT_INDICATOR_VI_NORMAL = { ") " }
+      # let-env PROMPT_MULTILINE_INDICATOR = { "::: " }
+    '' else "";
   varStr = lib.concatStringsSep "\n"
     (lib.mapAttrsToList (k: v: "let-env ${k} = \"${v}\"")
       config.home.sessionVariables) + "\n";
@@ -10,6 +24,7 @@ in
   programs.nushell = {
     enable = true;
     envFile.text =
+      PROMPT +
       varStr +
       LS_COLORS +
       builtins.readFile ./env.nu + "\n" +
