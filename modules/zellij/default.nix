0234a4c178474f55aa1b6790e6f045c7c1c1ace1
Zaechus <zaechus@proton.me>
2022-12-15 17:02:58 -0700

    fix zellij keybinds

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -7,16 +7,18 @@
       default_layout = ./layout.kdl;
 
       keybinds = {
-        unbind = [
-          { Ctrl = "b"; } # no tmux mode
-          { Ctrl = "h"; } # move
-          { Ctrl = "n"; } # resize
-          { Ctrl = "o"; } # session
-          { Ctrl = "p"; } # pane
-          { Ctrl = "s"; } # scroll
-          { Ctrl = "q"; } # no quit
-        ];
         normal = [
+          {
+            unbind = [
+              # { Ctrl = "b"; } # no tmux mode
+              { Ctrl = "h"; } # move
+              { Ctrl = "n"; } # resize
+              { Ctrl = "o"; } # session
+              { Ctrl = "p"; } # pane
+              { Ctrl = "s"; } # scroll
+              # { Ctrl = "q"; } # no quit
+            ];
+          }
           { key = [{ Ctrl = "y"; }]; action = [{ SwitchToMode = "Move"; }]; }
           { key = [{ Ctrl = "b"; }]; action = [{ SwitchToMode = "Resize"; }]; }
           { key = [{ Ctrl = "q"; }]; action = [{ SwitchToMode = "Session"; }]; }
