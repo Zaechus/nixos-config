328e0ad009c5289c578b1db32c9e0c20a8ddb8c6
Zaechus <zaechus@proton.me>
2023-02-11 18:32:11 -0700

    small change

--- users/zaechus.nix
+++ users/zaechus.nix
@@ -1,11 +1,13 @@
 {
-  home-manager.users.zaechus.programs.git = {
-    userName = "Zaechus";
-    userEmail = "zaechus@proton.me";
-  };
-
   users.users.zaechus = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+  };
+
+  home-manager.users.zaechus = {
+    programs.git = {
+      userName = "Zaechus";
+      userEmail = "zaechus@proton.me";
+    };
   };
 }
