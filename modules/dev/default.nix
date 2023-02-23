50d5067f705a0272d4993fbfe0fcddd8243110ce
Zaechus <zaechus@proton.me>
2023-02-23 14:18:44 -0700

    rust use mold

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -2,8 +2,6 @@
 
 {
   environment.systemPackages = with pkgs; [
-    clang # cc
-    llvmPackages.bintools # lld
     nil # Nix LSP
     tokei
   ];
--- modules/dev/rust/default.nix
+++ modules/dev/rust/default.nix
@@ -2,6 +2,8 @@
 
 {
   home.packages = with pkgs; [
+    clang
+    mold
     rustup
     rust-analyzer
     vscode-extensions.llvm-org.lldb-vscode # for hx
@@ -9,7 +11,7 @@
 
   home.file.".cargo/config.toml".text = ''
     [build]
-    rustflags = ["-Clink-arg=-fuse-ld=lld"]
+    rustflags = ["-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
   '';
 
   programs.neovim.plugins = with pkgs.vimPlugins; [
