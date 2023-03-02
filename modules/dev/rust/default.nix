611642c1bee01d1264fd7a9928e055a5ba529da5
Zaechus <zaechus@proton.me>
2023-03-02 15:20:21 -0700

    force clang linker in rust

--- modules/dev/rust/default.nix
+++ modules/dev/rust/default.nix
@@ -11,6 +11,7 @@
 
   home.file.".cargo/config.toml".text = ''
     [target.x86_64-unknown-linux-gnu]
+    linker = "clang"
     rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
   '';
 
