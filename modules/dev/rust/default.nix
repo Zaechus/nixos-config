bdd3c19c7718d6e48e5a5dbba90053b5316a7d4e
Zaechus <zaechus@proton.me>
2023-03-09 13:55:01 -0700

    config cargo to use new sparse protocol for registry

--- modules/dev/rust/default.nix
+++ modules/dev/rust/default.nix
@@ -13,6 +13,9 @@
     [target.x86_64-unknown-linux-gnu]
     linker = "clang"
     rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
+
+    [registries.crates-io]
+    protocol = "sparse"
   '';
 
   programs.neovim.plugins = with pkgs.vimPlugins; [
