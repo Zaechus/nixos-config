f3645a587cb0a9282b6142374afb7cc869dfcd51
Zaechus <zaechus@pm.me>
2023-09-21 19:04:46 -0600

    update rustatus

--- pkgs/rustatus/default.nix
+++ pkgs/rustatus/default.nix
@@ -8,7 +8,7 @@ rustPlatform.buildRustPackage rec {
     owner = "Zaechus";
     repo = pname;
     rev = "refs/heads/main";
-    hash = "sha256-fT4z+mZRXBoPR5Ur/P/5lfoOkcGMoYK+8muZ6YytKBw=";
+    hash = "sha256-wev2niXn9Z5MzWT59Kl8q82eU/lhNZY9hABHNqmD8oI=";
   };
 
   cargoHash = "sha256-NpVCKsCXqpuv+gSWpIHp28g/07F219cGi94yGxBbR5s=";
