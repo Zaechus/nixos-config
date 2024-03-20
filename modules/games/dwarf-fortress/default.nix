656a532f54e4e4d2d24d2b12ba0c7eef496695b1
Zaechus <zaechus@pm.me>
2024-03-20 14:36:32 -0600

    use dejavu sans mono for df

--- modules/games/dwarf-fortress/default.nix
+++ modules/games/dwarf-fortress/default.nix
@@ -11,9 +11,11 @@
 
   programs.nushell.extraConfig = ''
     def --wrapped dwarf-fortress [...args: string] {
+      alacritty msg config 'font.normal.family = "DejaVu Sans Mono"'
       alacritty msg config draw_bold_text_with_bright_colors=true
       ^dwarf-fortress
       alacritty msg config draw_bold_text_with_bright_colors=false
+      alacritty msg config 'font.normal.family = "monospace"'
     }
   '';
 }
