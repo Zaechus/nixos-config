a6d688a11bd6ec3daab69ba00116d7995dfdc16f
Zaechus <zaechus@pm.me>
2024-05-08 05:18:53 -0600

    use separate conf and map file for tie

--- modules/games/dosbox/default.nix
+++ modules/games/dosbox/default.nix
@@ -6,4 +6,6 @@
   ];
 
   home.file.".config/dosbox/dosbox-staging.conf".source = ./dosbox-staging.conf;
+  home.file.".config/dosbox/tie.conf".source = ./tie.conf;
+  home.file.".config/dosbox/tie.map".source = ./tie.map;
 }
