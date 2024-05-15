389173034a09c30941b3e2dd05bfd9926f18a2ec
Zaechus <zaechus@pm.me>
2024-05-15 16:08:38 -0600

    forgot to link dosbox default.conf; use separate conf for gauntlet

--- modules/games/dosbox/default.nix
+++ modules/games/dosbox/default.nix
@@ -6,6 +6,8 @@
   ];
 
   home.file.".config/dosbox/dosbox-staging.conf".source = ./dosbox-staging.conf;
+  home.file.".config/dosbox/default.conf".source = ./default.conf;
+  home.file.".config/dosbox/gauntlet.conf".source = ./gauntlet.conf;
   home.file.".config/dosbox/tie.conf".source = ./tie.conf;
   home.file.".config/dosbox/tie.map".source = ./tie.map;
 }
