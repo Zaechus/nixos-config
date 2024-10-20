25cd48ab72b88234a058f6e13bf1048e75812d32
Zaechus <zaechus@pm.me>
2024-10-20 16:16:30 -0600

    add new configs and maps to dosbox

--- modules/games/dosbox/default.nix
+++ modules/games/dosbox/default.nix
@@ -8,6 +8,8 @@
   home.file.".config/dosbox/dosbox-staging.conf".source = ./dosbox-staging.conf;
   home.file.".config/dosbox/default.conf".source = ./default.conf;
   home.file.".config/dosbox/gauntlet.conf".source = ./gauntlet.conf;
+  home.file.".config/dosbox/mk.conf".source = ./mk.conf;
+  home.file.".config/dosbox/mk.map".source = ./mk.map;
   home.file.".config/dosbox/tie.conf".source = ./tie.conf;
   home.file.".config/dosbox/tie.map".source = ./tie.map;
 }
