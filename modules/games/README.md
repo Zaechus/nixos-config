29682c3900e65584e420c0b9fc800cb5c602a178
Zaechus <zaechus@pm.me>
2024-12-31 17:08:44 -0700

    update flake; tiny 0.12.0 -> 0.13.0

--- modules/games/README.md
+++ modules/games/README.md
@@ -1,7 +1,12 @@
 # Games
 
-### rpcs3
+## Retroarch
+
+### N64
+`mupen64plus-parallel-rdp-deinterlace-method = "Weave"` is helpful for screenshake.
+
+## rpcs3
 Try enabling Write Color Buffers
 
-### xemu
+## xemu
 Try disabling Hard FPU emulation
--- pkgs/tiny/default.nix
+++ pkgs/tiny/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "tiny";
-  version = "0.12.0";
+  version = "0.13.0";
 
   src = fetchFromGitHub {
     owner = "osa1";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-VlKhOHNggT+FbMvE/N2JQOJf0uB1N69HHdP09u89qSk=";
+    hash = "sha256-phjEae2SS3zkSpuhhE4iscUM8ij8DT47YLIMATMG/+Q=";
   };
 
-  cargoHash = "sha256-rGsGPiZ3b4KRywzlJm/u+1BRBq5oECe6gDOVLUHjCzo=";
+  cargoHash = "sha256-DvFrRH6wObMvkRxS8vh3tlCrZNAiuZHfN0ARagMfG2k=";
 
   meta = with lib; {
     description = "A terminal IRC client written in Rust";
