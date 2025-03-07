c9a3a5671a64f10c81ba498c95de0137520bb503
Zaechus <zaechus@pm.me>
2025-03-07 14:21:43 -0700

    fix wg-quick after sleep

--- common/network/wg-quick.nix
+++ common/network/wg-quick.nix
@@ -8,7 +8,7 @@
   # https://github.com/WireGuard/wireguard-tools/blob/master/src/systemd/wg-quick%40.service
   systemd.services.wg-quick = {
     description = "WireGuard via wg-quick(8) for wg0";
-    wantedBy = [ "multi-user.target" ];
+    # wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" "nss-lookup.target" ];
     wants = [ "network-online.target" "nss-lookup.target" ];
     serviceConfig = {
@@ -17,10 +17,13 @@
       ExecStart = "${pkgs.wireguard-tools}/bin/wg-quick up wg0";
       ExecStartPost = "resolvectl dnsovertls wg0 no";
       ExecStop = "${pkgs.wireguard-tools}/bin/wg-quick down wg0";
+      ExecReload = "${pkgs.bash}/bin/bash -c 'exec ${pkgs.wireguard-tools}/bin/wg syncconf wg0 <(exec ${pkgs.wireguard-tools}/bin/wg-quick strip wg0)'";
       Environment = "WG_ENDPOINT_RESOLUTION_RETRIES=infinity";
     };
     unitConfig = {
       ConditionPathExists = "/etc/wireguard/wg0.conf";
     };
   };
+
+  systemd.network.config.networkConfig.ManageForeignRoutingPolicyRules = false;
 }
