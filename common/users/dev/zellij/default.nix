30ea89ec644d1364339e94f7f5315313b7bd6ad9
Zaechus <zaechus@pm.me>
2025-12-25 15:27:05 -0800

    disable session_serialization in zellij

--- common/users/dev/zellij/default.nix
+++ common/users/dev/zellij/default.nix
@@ -9,6 +9,7 @@
       default_shell = lib.mkDefault "nu";
       pane_frames = false;
       scrollback_editor = lib.mkDefault "hx";
+      session_serialization = false;
       simplified_ui = true;
 
       keybinds = builtins.readFile ./keybinds.kdl;
@@ -24,6 +25,7 @@
       default_shell "${default_shell}"
       pane_frames ${if pane_frames then "true" else "false"}
       scrollback_editor "${scrollback_editor}"
+      session_serialization ${if session_serialization then "true" else "false"}
       simplified_ui ${if simplified_ui then "true" else "false"}
 
       keybinds {
